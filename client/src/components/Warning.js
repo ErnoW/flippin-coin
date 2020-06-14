9dca6cb61f161a3fe3c750a022ba5fb53af489a3
Erno Wever <ernowever@gmail.com>
2020-06-14 19:52:50 +0200

    feat: improve error message

--- client/src/components/Warning.js
+++ client/src/components/Warning.js
@@ -29,14 +29,16 @@ export const Warning = () => {
   const isReady = useRenderTimeout();
   let warning = null;
 
+  const supportedChainNames = SUPPORTED_CHAINS.map(
+    (id) => chains[id].name,
+  ).join(", ");
+
   if (!isMetaMask) {
     warning = "No MetaMask found, MetaMask is required to interact.";
   } else if (!isActive) {
-    warning = "Connect your wallet to start";
+    warning = `Connect your wallet to start, supported chains: ${supportedChainNames}`;
   } else if (!isValidChainId(chainId)) {
-    warning = `Invalid chain, supported chains: ${SUPPORTED_CHAINS.map(
-      (id) => chains[id].name,
-    ).join(", ")}`;
+    warning = `Invalid chain, supported chains: ${supportedChainNames}`;
   }
 
   if (!warning || !isReady) {
